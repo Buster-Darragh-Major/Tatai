8d8d2afd612142d373acb20f63435dc3028c7c69
Buster Major <bmaj406@aucklanduni.ac.nz>
2017-09-05 12:39:39 +1200

    progress on main menu and scene swapping achieved between views

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -1,14 +1,32 @@
 package gui;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainWindow extends Application {
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+		    
+	        Scene scene = new Scene(root, 600, 400);
+	    
+	        primaryStage.setTitle("Tatai");
+	        primaryStage.setScene(scene);
+	        primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+	
+	public static void main(String[] args) {
+		launch(args);
+}
 
 }
--- src/gui/MainWindowController.java
+++ src/gui/MainWindowController.java
@@ -1,9 +1,38 @@
 package gui;
 
-import com.jfoenix.controls.JFXHamburger;
+import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class MainWindowController {
 	
+	// Main Menu Buttons
+	@FXML
+	private Button _mainMenuPlay;
+	@FXML
+	private Button _mainMenuStats;
+	@FXML
+	private Button _mainMenuQuit;
+	
+	@FXML
+	public void handleStatsMenuPlayClick() {
+		Parent root = null;
+		try {
+			// Load root .fxml file for stats view
+			root = FXMLLoader.load(getClass().getResource("StatsWindow.fxml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// Get current stage
+		Stage stage = (Stage) _mainMenuStats.getScene().getWindow();
+		
+		Scene scene = new Scene(root); // Create new scene based off stats view root
+		stage.setScene(scene); // Set current stage to show new stats view scene
+		stage.show();
+	}
 }
