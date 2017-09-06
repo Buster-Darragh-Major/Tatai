e42754db93dafce3161cbdf5f5d973cabfd89c5d
Nathan Cairns <nathancairns777@gmail.com>
2017-09-06 12:46:48 +1200

    created statistic handler interface

--- src/stats/StatisticHandler.java
+++ src/stats/StatisticHandler.java
@@ -1,5 +1,20 @@
 package stats;
 
-public class StatisticHandler {
+/**
+ * Interface to represent a type which returns statistics
+ * 
+ * @author Nathan Cairns
+ *
+ */
+public interface StatisticHandler {
 
+	public int totalPlayer();
+	
+	public int totalIncorrect();
+	
+	public int totalCorrect();
+	
+	public int average();
+	
+	public int update(int played, int incorrect, int correct);
 }
