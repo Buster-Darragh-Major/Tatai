d664c813a8e7ae1a2bc760252db697e21b61b275
Buster Major <bmaj406@aucklanduni.ac.nz>
2017-09-07 15:20:10 +1200

    added some creation model logic and a creation builder class with distinct settings for level 1 and 2

--- src/creations/tatai/TataiCreationModel.java
+++ src/creations/tatai/TataiCreationModel.java
@@ -1,14 +1,34 @@
 package creations.tatai;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import creations.cr.Creation;
 import creations.ml.CreationModel;
+import creations.tatai.TataiCreationBuilder.Level;
 
 public class TataiCreationModel extends CreationModel{
+	
+	private List<Creation> _creations = new ArrayList<Creation>();
+	private Level _level;
+	
+	// Constructor, default sets to level 1
+	public TataiCreationModel() {
+		_level = Level.Level1;
+	}
 
+	public void setLevel(Level level) {
+		_level = level;
+	}
+	
+	public void populateModel() {
+		TataiCreationBuilder b = new TataiCreationBuilder();
+		_creations.addAll(b.populateList(_level));
+	}
+	
 	@Override
 	public void addCreation(Creation creation) {
-		// TODO Auto-generated method stub
-		
+		_creations.add(creation);
 	}
 
 	@Override
--- src/gui/LevelSelectWindowController.java
+++ src/gui/LevelSelectWindowController.java
@@ -1,7 +1,14 @@
 package gui;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import com.jfoenix.controls.JFXHamburger;
 
+import creations.tatai.TataiCreation;
+import creations.tatai.TataiCreationBuilder.Level;
+import creations.tatai.TataiCreationModel;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
@@ -15,6 +22,8 @@ public class LevelSelectWindowController extends TataiController {
 	@FXML
 	private Button _level2;
 	
+	private TataiCreationModel _model;
+	
 	@FXML
 	public void handleMainMenuCLick() {
 		Stage stage = (Stage) _mainMenu.getScene().getWindow(); // Get current stage
@@ -23,12 +32,16 @@ public class LevelSelectWindowController extends TataiController {
 	
 	@FXML
 	public void handleLevel1Click() {
-		
+		_model = new TataiCreationModel();
+		_model.setLevel(Level.Level1);
+		_model.populateModel();
 	}
 	
 	@FXML
 	public void handleLevel2Click() {
-		
+		_model = new TataiCreationModel();
+		_model.setLevel(Level.Level2);
+		_model.populateModel();
 	}
 	
 }
