dc31f1cb8f291649c1d4472f3ddc96ddf2700d8a
Buster Major <bmaj406@aucklanduni.ac.nz>
2017-09-12 21:19:34 +1200

    Moved inner Question class to Separate TataiQuestion file

--- src/gui/ResultsWindowController.java
+++ src/gui/ResultsWindowController.java
@@ -14,15 +14,16 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
+import stats.TataiQuestion;
 
 public class ResultsWindowController extends TataiController implements Initializable {
 
 	@FXML
 	private Label _scoreLabel;
 	@FXML
-	private TableView<Question> _resultsTable;
+	private TableView<TataiQuestion> _resultsTable;
 	@FXML
-	private TableColumn<Question, String> qNo, qInt, qTranslation, qCorrect;
+	private TableColumn<TataiQuestion, String> qNo, qInt, qTranslation, qCorrect;
 	
 	
 	@SuppressWarnings("static-access")
@@ -41,84 +42,17 @@ public class ResultsWindowController extends TataiController implements Initiali
 		ArrayList<String> trans = Context.getInstance().currentGame().getQuestionTrans();
 		ArrayList<String> correct = Context.getInstance().currentGame().getQuestionCorrect();
 		for (int i = 0; i < _questionTotal; i++) {
-			_resultsTable.getItems().add(new Question(i + 1 + "", ints.get(i), trans.get(i), correct.get(i)));
+			_resultsTable.getItems().add(new TataiQuestion(i + 1 + "", ints.get(i), trans.get(i), correct.get(i)));
 		}
 		
 		// Set up Columns
-		qNo.setCellValueFactory(new PropertyValueFactory<Question, String>("qNo"));
-		qInt.setCellValueFactory(new PropertyValueFactory<Question, String>("qInt"));
-		qTranslation.setCellValueFactory(new PropertyValueFactory<Question, String>("qTranslation"));
-		qCorrect.setCellValueFactory(new PropertyValueFactory<Question, String>("qCorrect"));
+		qNo.setCellValueFactory(new PropertyValueFactory<TataiQuestion, String>("qNo"));
+		qInt.setCellValueFactory(new PropertyValueFactory<TataiQuestion, String>("qInt"));
+		qTranslation.setCellValueFactory(new PropertyValueFactory<TataiQuestion, String>("qTranslation"));
+		qCorrect.setCellValueFactory(new PropertyValueFactory<TataiQuestion, String>("qCorrect"));
 			
 		colorRows();
 	}
-	
-	
-	
-	
-	public static class Question {
-		
-		private final SimpleStringProperty qNo;
-		private final SimpleStringProperty qInt;
-		private final SimpleStringProperty qTranslation;
-		private final SimpleStringProperty qCorrect;
-		
-		public Question(String questNo, String questInt, String questTranslation, String questCorrect) {
-			qNo = new SimpleStringProperty(questNo);
-			qInt = new SimpleStringProperty(questInt);
-			qTranslation = new SimpleStringProperty(questTranslation);
-			qCorrect = new SimpleStringProperty(questCorrect);
-		}
-		
-		public StringProperty qNoProperty() {
-			return qNo;
-		}
-		
-		public String getQNo() {
-			return qNo.get();
-		}
-		
-		public void setQNo(String questNo) {
-			qNo.set(questNo);
-		}
-		
-		public StringProperty qIntProperty() {
-			return qInt;
-		}
-		
-		public String getQInt() {
-			return qInt.get();
-		}
-		
-		public void setQInt(String questInt) {
-			qInt.set(questInt);
-		}
-		
-		public StringProperty qTranslationProperty() {
-			return qTranslation;
-		}
-		
-		public String getQTranslation() {
-			return qTranslation.get();
-		}
-		
-		public void setQTranslation(String questTranslation) {
-			qTranslation.set(questTranslation);
-		}
-		
-		public StringProperty qCorrectProperty() {
-			return qCorrect;
-		}
-		
-		public String getQCorrect() {
-			return qCorrect.get();
-		}
-		
-		public void setQCorrect(String questCorrect) {
-			qCorrect.set(questCorrect);
-		}
-	}
-	
 	
 	
 	private void colorRows() {
