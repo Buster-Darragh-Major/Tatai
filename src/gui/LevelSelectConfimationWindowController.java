af86c34863af1c1f878e765e0c482ffc61b85bab
Nathan Cairns <nathancairns777@gmail.com>
2017-09-12 18:47:28 +1200

    removed hamburger menu

--- src/gui/LevelSelectConfimationWindowController.java
+++ src/gui/LevelSelectConfimationWindowController.java
@@ -3,8 +3,6 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import com.jfoenix.controls.JFXHamburger;
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -17,8 +15,6 @@ public class LevelSelectConfimationWindowController extends TataiController impl
 	private Button _start;
 	@FXML
 	private Button _back;
-	@FXML
-	private JFXHamburger _mainMenu;
 	@FXML
 	private Label _levelHeader;
 	@FXML
@@ -43,12 +39,6 @@ public class LevelSelectConfimationWindowController extends TataiController impl
 	public void handleBackClick() {
 		Stage stage = (Stage) _back.getScene().getWindow(); // Get current stage
 		changeWindow("LevelSelectWindow.fxml", stage); // Change to LevelSelectWindow.fxml view
-	}
-	
-	@FXML
-	public void handleMainMenuClick() {
-		Stage stage = (Stage) _mainMenu.getScene().getWindow(); // Get current stage
-		changeWindow("MainWindow.fxml", stage); // Change to MainWindow.fxml view
 	}
 
 	@Override
--- src/gui/LevelSelectWindowController.java
+++ src/gui/LevelSelectWindowController.java
@@ -1,7 +1,5 @@
 package gui;
 
-import com.jfoenix.controls.JFXHamburger;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
@@ -9,18 +7,10 @@ import tatai.creations.Level;
 
 public class LevelSelectWindowController extends TataiController {
 
-	@FXML
-	private JFXHamburger _mainMenu;
 	@FXML 
 	private Button _level1;
 	@FXML
 	private Button _level2;
-	
-	@FXML
-	public void handleMainMenuCLick() {
-		Stage stage = (Stage) _mainMenu.getScene().getWindow(); // Get current stage
-		changeWindow("MainWindow.fxml", stage); // Change to MainWindow.fxml view
-	}
 	
 	@FXML
 	public void handleLevel1Click() {
