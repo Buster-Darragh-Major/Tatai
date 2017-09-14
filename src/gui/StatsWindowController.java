394a73c46be215b0ca6504a6ce9c9e21ef64cb58
Nathan Cairns <nathancairns777@gmail.com>
2017-09-14 15:15:40 +1200

    Made resizable

--- src/gui/StatsWindowController.java
+++ src/gui/StatsWindowController.java
@@ -7,7 +7,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 import tatai.game.TataiGame;
