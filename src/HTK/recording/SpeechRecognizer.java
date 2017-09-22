ff304a68fe77cec2b7e65663ab820b4036ac146e
Buster Major <bmaj406@aucklanduni.ac.nz>
2017-09-22 17:10:21 +1200

    implemented record method in SpeechRecognizer

--- src/HTK/recording/SpeechRecognizer.java
+++ src/HTK/recording/SpeechRecognizer.java
@@ -1,6 +1,7 @@
 package HTK.recording;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class SpeechRecognizer {
@@ -9,8 +10,23 @@ public class SpeechRecognizer {
 			System.getProperty("user.dir") + System.getProperty("file.separator") + "HTK" +
 			System.getProperty("file.separator") + "MaoriNumbers");
 	
+	private ArrayList<String> output;
+	
 	public void record() {
+		String command = "./GoSpeech";
 		
+		// Build a builder with relevant bash command line command
+		ProcessBuilder builder = new ProcessBuilder("bin/bash", "-c", command);
+		
+		// Change working location of builder to work in the directory containing HTK sript
+		builder.directory(FILEPATH);
+		
+		try {
+			Process process = builder.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public ArrayList<String> getText() {
