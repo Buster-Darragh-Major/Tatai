19feb575381dc265ad0e642d0ec1ac9de91865d4
Nathan Cairns <nathancairns777@gmail.com>
2017-09-22 18:19:57 +1200

    optimize imports

--- src/gui/GameWindowController.java
+++ src/gui/GameWindowController.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
