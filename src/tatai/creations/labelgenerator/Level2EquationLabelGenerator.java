b84b7fa1814a695baba72578fa278e2eca37f610
Nathan Cairns <nathancairns777@gmail.com>
2017-10-11 21:18:58 +1300

    get random division from non prime list

--- src/tatai/creations/labelgenerator/Level2EquationLabelGenerator.java
+++ src/tatai/creations/labelgenerator/Level2EquationLabelGenerator.java
@@ -8,6 +8,7 @@ public class Level2EquationLabelGenerator extends Level1EquationLabelGenerator {
 	/* Macros */
 	public static final int MIN = 1;
 	public static final int MAX = 99;
+	public static final int[] NON_PRIMES = {2,4,6,8,9,10,12};
 	
 	@Override
 	public void chooseOperator(int maximum, int minimum) {
@@ -31,10 +32,9 @@ public class Level2EquationLabelGenerator extends Level1EquationLabelGenerator {
 	
 	
 	protected void generateDivision(int maximum , int minimum) {
-		int operand1 = getRandomInteger(maximum, 2 * minimum);
-		while (isPrime(operand1)) {
-			operand1 = getRandomInteger(maximum, 2 * minimum);
-		}
+		int rand = (int) (NON_PRIMES.length * Math.random());
+		
+		int operand1 = NON_PRIMES[rand];
 		
 		// create list of divisible numbers by i
 		List<Integer> operand2Range = new ArrayList<Integer>();
@@ -49,14 +49,4 @@ public class Level2EquationLabelGenerator extends Level1EquationLabelGenerator {
 		
 		_label = operand1 + DIVISION + operand2 + EQUALS;
 	}
-	
-	private static boolean isPrime(int num) {
-        if (num < 2) return false;
-        if (num == 2) return true;
-        if (num % 2 == 0) return false;
-        for (int i = 3; i * i <= num; i += 2)
-            if (num % i == 0) return false;
-        return true;
-	}
-	
 }
