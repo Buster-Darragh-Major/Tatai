09cc65d7a1f1685ab8997b8fcf419e12c790341f
Buster Major <bmaj406@aucklanduni.ac.nz>
2017-10-15 14:05:39 +1300

    added teacherinputwindow view

---
+++ src/gui/TeacherInputWindowController.java
@@ -0,0 +1,37 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class TeacherInputWindowController {
+
+	/* FXML Nodes */
+	@FXML
+	private Label _inputLabel;
+	@FXML
+	private Button _0;
+	@FXML
+	private Button _1;
+	@FXML
+	private Button _2;
+	@FXML
+	private Button _3;
+	@FXML
+	private Button _4;
+	@FXML
+	private Button _5;
+	@FXML
+	private Button _6;
+	@FXML
+	private Button _7;
+	@FXML
+	private Button _8;
+	@FXML
+	private Button _9;
+	@FXML
+	private Button _enterButton;
+	@FXML
+	private Button _exitButton;
+	
+}
