b7be633f846602bc40d065ca606d82fe4d202ada
Buster Major <bmaj406@aucklanduni.ac.nz>
2017-10-20 16:08:22 +1300

    edit lists button

--- src/gui/TeacherInputNamingWindowController.java
+++ src/gui/TeacherInputNamingWindowController.java
@@ -1,21 +1,28 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXTextField;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import questionlist.TextQuestionListHandler;
 
 public class TeacherInputNamingWindowController extends TataiController implements Initializable {
 
 	@FXML private Button _exitButton;
 	@FXML private Button _createButton;
+	@FXML private Button _editButton;
 	@FXML private Label _warningLabel;
 	@FXML private JFXTextField _textField;
 	
@@ -23,6 +30,24 @@ public class TeacherInputNamingWindowController extends TataiController implemen
 	public void handleExitCick() {
 		Stage stage = (Stage) _exitButton.getScene().getWindow(); // Get Current stage
 		changeWindow("MainWindow.fxml", stage);// Change to MainWindow.fxml view
+	}
+	
+	@FXML
+	public void handleEditClick() {
+		try {
+			FXMLLoader fxml = new FXMLLoader(getClass().getResource("CustomListEditView.fxml"));
+			Parent root = (Parent) fxml.load();
+			Stage stage = new Stage();
+			stage.initModality(Modality.APPLICATION_MODAL);
+			Scene scene = new Scene(root);
+			stage.setTitle("Edit Lists");
+			stage.setScene(scene);
+			stage.initStyle(StageStyle.UNDECORATED);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	@FXML
