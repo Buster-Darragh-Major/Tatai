7621e2511d9d73c3c3bf7f819df71ddabb55351e
Nathan Cairns <nathancairns777@gmail.com>
2017-10-21 15:40:21 +1300

    student skill level implemented

--- src/users/user/Student.java
+++ src/users/user/Student.java
@@ -2,40 +2,115 @@ package users.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import game.Level;
+import stats.Stat;
+import stats.StatSkill;
 import stats.TataiStat;
 
 /**
- * This class represents a student user.
- * A student user does not have writing privileges and therefore cannot
- * create custom lists.
- * A student can also have achievments. //TODO
+ * This class represents a student user. A student user does not have writing
+ * privileges and therefore cannot create custom lists. A student can also have
+ * achievments. //TODO
  * 
  * @author Nathan Cairns
  *
  */
 public class Student extends User {
-	
+
 	/**
 	 * Constructor
 	 * 
-	 * @param firstName the first name of the user
-	 * @param LastName the last name of the user
-	 * @param userName the user name the user has choosen
+	 * @param firstName
+	 *            the first name of the user
+	 * @param LastName
+	 *            the last name of the user
+	 * @param userName
+	 *            the user name the user has choosen
 	 */
 	public Student(String firstName, String lastName, String userName) throws UserException {
 		super(firstName, lastName, userName);
 		_writingPrivileges = false;
 	}
-	
 
 	/**
 	 * Constructor for Student with pre existing stats
 	 */
-	public Student(@JsonProperty("_firstName")String firstName, @JsonProperty("_lastName") String lastName,
-			@JsonProperty("_userName")String userName, @JsonProperty("_lvl1Stats") TataiStat stat1,
+	public Student(@JsonProperty("_firstName") String firstName, @JsonProperty("_lastName") String lastName,
+			@JsonProperty("_userName") String userName, @JsonProperty("_lvl1Stats") TataiStat stat1,
 			@JsonProperty("_lvl2Stats") TataiStat stat2) {
 		super(firstName, lastName, userName, stat1, stat2);
 		_writingPrivileges = false;
+	}
+
+	/**
+	 * Determines the users skill level for a given stat and level.
+	 * 
+	 * @param stat
+	 *            the stat to enquire about
+	 * @param level
+	 *            the game level of the stat you are interested in
+	 * @return the skill level of that stat for a given level.
+	 */
+	public StatSkill getStatSkill(Stat stat, Level level) {
+		int value = 0;
+
+		switch (stat) {
+		case TOTALPLAYED:
+			value = getTotalPlayed(level);
+			break;
+		case TOTALCORRECT:
+			value = getTotalCorrect(level);
+			break;
+		case TOTALINCORRECT:
+			value = getTotalIncorrect(level);
+			break;
+		case AVERAGE:
+			return determineAverageSkill(getAverage(level));
+		}
+		return determineTotalSkill(value);
+	}
+
+	/**
+	 * Determines the the stat skill level for the three "total" stats. i.e Total
+	 * played, total correct & total incorrect.
+	 * 
+	 * @param value
+	 *            the value of the particular stat.
+	 * @return the skill level
+	 */
+	protected StatSkill determineTotalSkill(int value) {
+		if (value >= 25 && value < 50) {
+			return StatSkill.BRONZE;
+		} else if (value >= 50 && value < 100) {
+			return StatSkill.SILVER;
+		} else if (value >= 100 && value < 1000) {
+			return StatSkill.GOLD;
+		} else if (value >= 1000) {
+			return StatSkill.PLATINUM;
+		} else {
+			return StatSkill.NONE;
+		}
+	}
+
+	/**
+	 * Determines the stat skill level for the students average.
+	 * 
+	 * @param value
+	 *            the percent the student's average is currently on
+	 * @return the stat skill level of the student's average
+	 */
+	protected StatSkill determineAverageSkill(Double value) {
+		if (value >= 60 && value < 75) {
+			return StatSkill.BRONZE;
+		} else if (value >= 75 && value < 85) {
+			return StatSkill.SILVER;
+		} else if (value >= 85 && value < 95) {
+			return StatSkill.GOLD;
+		} else if (value >= 95 && value <= 100) {
+			return StatSkill.PLATINUM;
+		} else {
+			return StatSkill.NONE;
+		}
 	}
 
 }
